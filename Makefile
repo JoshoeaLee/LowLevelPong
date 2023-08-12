3a1c4cb68a70bc21f59615e50d6447e1ed5ef8b9
Joshua Lee <joshleecloud@gmail.com>
2023-08-12 15:07:16 +1200

    Reconfigured TTF

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 game: 
-	clang++ main.cpp -o play -I include -L lib -l SDL2-2.0.0 -l SDL2_image -l SDL2_ttf
+	clang++ main.cpp -o play -I include -L lib -l SDL2-2.0.0 -l SDL2_image-2.0.0 -l SDL2_ttf-2.0.0
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_ttf.h>
 #include <iostream>
+
+SDL_Renderer *renderer;
+SDL_Window *window;
+SDL_Color color;
 
 int main(int argc, char *argv[])
 {
