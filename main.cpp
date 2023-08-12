97ac6275c431bf337107e1e64543d901f3da2150
Joshua Lee <joshleecloud@gmail.com>
2023-08-12 14:50:52 +1200

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
